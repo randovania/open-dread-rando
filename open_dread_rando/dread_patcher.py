4879e6db2be41323b3658af89f751432fc20dc6c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-24 17:44:47 +0200

    Rafactoring and basic test

--- open_dread_rando/dread_patcher.py
+++ open_dread_rando/dread_patcher.py
@@ -13,45 +13,56 @@ def _read_schema():
         return json.load(f)
 
 
+def create_init_copy(editor: PkgEditor):
+    original_init = "system/scripts/original_init.lc"
+    if not editor.does_asset_exists(original_init):
+        original_lc = editor.get_raw_asset("system/scripts/init.lc")
+        editor.add_new_asset(
+            original_init,
+            original_lc,
+            editor.find_pkgs("system/scripts/init.lc")
+        )
+
+
+def create_custom_init(inventory: dict[str, int], starting_location: dict):
+    return """
+    Game.ImportLibrary("system/scripts/original_init.lua")
+
+    Init.tNewGameInventory = {{ 
+    {new_game_inventory}
+    }}
+
+    Game.LogWarn(0, "Inventory:")
+    for k, v in pairs(Init.tNewGameInventory) do
+        Game.LogWarn(0, tostring(k) .. " = " .. tostring(v))
+    end
+
+    function Game.StartPrologue(arg1, arg2, arg3, arg4, arg4)
+        Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
+        Game.LoadScenario("c10_samus", "{starting_level}", "{starting_actor}", "", 1)
+    end
+
+    Game.LogWarn(0, "Finished modded system/init.lc")
+
+    """.format(
+        new_game_inventory="\n".join(
+            "{} = {},".format(key, value)
+            for key, value in inventory.items()
+        ),
+        starting_level=starting_location["level"],
+        starting_actor=starting_location["actor"],
+    )
+
+
 def patch(input_path: Path, output_path: Path, configuration: dict):
     LOG.info("Will patch files at %s", input_path)
 
     jsonschema.validate(instance=configuration, schema=_read_schema())
 
+    out_romfs = output_path.joinpath("romfs")
     editor = PkgEditor(input_path)
 
-    if not editor.does_asset_exists("system/scripts/original_init.lc"):
-        original_lc = editor.get_raw_asset("system/scripts/init.lc")
-        editor.replace_asset("system/scripts/original_init.lc", original_lc)
-
-    # inventory = {
-    #     "ITEM_MAX_LIFE": 99,
-    #     "ITEM_MAX_SPECIAL_ENERGY": 1000,
-    #     "ITEM_WEAPON_MISSILE_MAX": 15,
-    #     "ITEM_WEAPON_POWER_BOMB_MAX": 0,
-    #     "ITEM_METROID_COUNT": 0,
-    #     "ITEM_METROID_TOTAL_COUNT": 40,
-    #     "ITEM_FLOOR_SLIDE": 1,
-    # }
-
-    custom_init = """
-Game.ImportLibrary("system/scripts/init.lua")
-
-Init.tNewGameInventory = {{ 
-{new_game_inventory}
-}}
-
-function Game.StartPrologue(arg1, arg2, arg3, arg4, arg4)
-    Game.LoadScenario("c10_samus", "{starting_level}", "{starting_actor}", "", 1)
-end
-""".format(
-        new_game_inventory="\n".join(
-            "{} = {},".format(key, value)
-            for key, value in configuration["starting_items"].items()
-        ),
-        starting_level=configuration["starting_location"]["level"],
-        starting_actor=configuration["starting_location"]["actor"],
-    )
-
+    create_init_copy(editor)
+    custom_init = create_custom_init(configuration["starting_items"], configuration["starting_location"])
     editor.replace_asset("system/scripts/init.lc", custom_init.encode("ascii"))
-    editor.save_modified_pkgs(output_path)
+    editor.save_modified_pkgs(out_romfs)
