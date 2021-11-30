f1b99bbfe142f28cceeda3dcb8127df31e820d01
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-30 03:14:45 +0200

    Support random elevators

---
+++ open_dread_rando/custom_init.lua
@@ -0,0 +1,19 @@
+Game.ImportLibrary("system/scripts/original_init.lua")
+
+Init.tNewGameInventory = {
+    TEMPLATE("new_game_inventory")
+}
+
+Game.LogWarn(0, "Inventory:")
+for k, v in pairs(Init.tNewGameInventory) do
+    Game.LogWarn(0, tostring(k) .. " = " .. tostring(v))
+end
+
+local buff = {}
+
+function Game.StartPrologue(arg1, arg2, arg3, arg4, arg4)
+    Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
+    Game.LoadScenario("c10_samus", TEMPLATE("starting_scenario"), TEMPLATE("starting_actor"), "", 1)
+end
+
+Game.LogWarn(0, "Finished modded system/init.lc")
--- open_dread_rando/dread_patcher.py
+++ open_dread_rando/dread_patcher.py
@@ -1,10 +1,14 @@
 import json
 import logging
+import typing
 from pathlib import Path
 
 import jsonschema
-from mercury_engine_data_structures.pkg_editor import PkgEditor
+from mercury_engine_data_structures.file_tree_editor import FileTreeEditor
+from mercury_engine_data_structures.formats import Brfld, BaseResource
+from mercury_engine_data_structures.formats.dread_types import EBreakableTileType
 
+T = typing.TypeVar("T")
 LOG = logging.getLogger("dread_patcher")
 
 
@@ -13,7 +17,11 @@ def _read_schema():
         return json.load(f)
 
 
-def create_init_copy(editor: PkgEditor):
+def path_for_level(level_name: str) -> str:
+    return f"maps/levels/c10_samus/{level_name}/{level_name}"
+
+
+def create_init_copy(editor: FileTreeEditor):
     original_init = "system/scripts/original_init.lc"
     if not editor.does_asset_exists(original_init):
         original_lc = editor.get_raw_asset("system/scripts/init.lc")
@@ -25,33 +33,56 @@ def create_init_copy(editor: PkgEditor):
 
 
 def create_custom_init(inventory: dict[str, int], starting_location: dict):
-    return """
-    Game.ImportLibrary("system/scripts/original_init.lua")
+    def _wrap(v: str):
+        return f'"{v}"'
 
-    Init.tNewGameInventory = {{ 
-    {new_game_inventory}
-    }}
-
-    Game.LogWarn(0, "Inventory:")
-    for k, v in pairs(Init.tNewGameInventory) do
-        Game.LogWarn(0, tostring(k) .. " = " .. tostring(v))
-    end
-
-    function Game.StartPrologue(arg1, arg2, arg3, arg4, arg4)
-        Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
-        Game.LoadScenario("c10_samus", "{starting_level}", "{starting_actor}", "", 1)
-    end
-
-    Game.LogWarn(0, "Finished modded system/init.lc")
-
-    """.format(
-        new_game_inventory="\n".join(
+    replacement = {
+        "new_game_inventory": "\n".join(
             "{} = {},".format(key, value)
             for key, value in inventory.items()
         ),
-        starting_level=starting_location["level"],
-        starting_actor=starting_location["actor"],
-    )
+        "starting_scenario": _wrap(starting_location["scenario"]),
+        "starting_actor": _wrap(starting_location["actor"]),
+    }
+
+    code = Path(__file__).parent.joinpath("custom_init.lua").read_text()
+    for key, content in replacement.items():
+        code = code.replace(f'TEMPLATE("{key}")', content)
+
+    return code
+
+
+class PatcherEditor(FileTreeEditor):
+    memory_files: dict[str, BaseResource]
+
+    def __init__(self, root: Path):
+        super().__init__(root)
+        self.memory_files = {}
+
+    def get_file(self, path: str, type_hint: typing.Type[T] = BaseResource) -> T:
+        if path not in self.memory_files:
+            self.memory_files[path] = self.get_parsed_asset(path, type_hint=type_hint)
+        return self.memory_files[path]
+
+    def get_scenario(self, name: str) -> Brfld:
+        return self.get_file(path_for_level(name) + ".brfld", Brfld)
+
+    def flush_modified_assets(self):
+        for name, resource in self.memory_files.items():
+            self.replace_asset(name, resource)
+        self.memory_files = {}
+
+
+def patch_elevators(editor: PatcherEditor, elevators_config: list[dict]):
+    for elevator in elevators_config:
+        level = editor.get_scenario(elevator["teleporter"]["scenario"])
+        actor = level.actors_for_layer(elevator["teleporter"]["layer"])[elevator["teleporter"]["actor"]]
+        try:
+            usable = actor.pComponents.USABLE
+        except AttributeError:
+            raise ValueError(f'Actor {elevator["teleporter"]} is not a teleporter')
+        usable.sScenarioName = elevator["destination"]["scenario"]
+        usable.sTargetSpawnPoint = elevator["destination"]["actor"]
 
 
 def patch(input_path: Path, output_path: Path, configuration: dict):
@@ -60,9 +91,27 @@ def patch(input_path: Path, output_path: Path, configuration: dict):
     jsonschema.validate(instance=configuration, schema=_read_schema())
 
     out_romfs = output_path.joinpath("romfs")
-    editor = PkgEditor(input_path)
+    editor = PatcherEditor(input_path)
 
     create_init_copy(editor)
-    custom_init = create_custom_init(configuration["starting_items"], configuration["starting_location"])
-    editor.replace_asset("system/scripts/init.lc", custom_init.encode("ascii"))
+
+    editor.replace_asset(
+        "system/scripts/init.lc",
+        create_custom_init(configuration["starting_items"],
+                           configuration["starting_location"]
+                           ).encode("ascii"))
+
+    # actor = level.actors_for_layer("default")[configuration["starting_location"]["actor"]]
+    # old_on_teleport = actor.pComponents["STARTPOINT"]["sOnTeleport"]
+    # if old_on_teleport not in ("", "Game.HUDIdleScreenLeave"):
+    #     raise ValueError("Starting actor at {} with unexpected sOnTeleport: {}".format(
+    #         configuration["starting_location"], old_on_teleport,
+    #     ))
+    # actor.pComponents["STARTPOINT"]["sOnTeleport"] = "Game.HUDIdleScreenLeave"
+
+    if "elevators" in configuration:
+        patch_elevators(editor, configuration["elevators"])
+
+    editor.flush_modified_assets()
     editor.save_modified_pkgs(out_romfs)
+    logging.info("Done")
