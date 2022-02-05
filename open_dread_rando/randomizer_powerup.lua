79e3c865285305c97083f05c67f70f8e9a9e3cbd
duncathan <dunc@duncathan.com>
2022-02-04 21:02:16 -0600

    fix etanks on bosses and PBs

--- open_dread_rando/randomizer_powerup.lua
+++ open_dread_rando/randomizer_powerup.lua
@@ -13,6 +13,8 @@ function RandomizerPowerup.OnPickedUp(actor, progression)
     Game.LogWarn(0, "Collected pickup: " .. name)
     local granted = RandomizerPowerup.ProgressivePickup(progression)
     RandomizerPowerup.ChangeSuit()
+    RandomizerPowerup.IncreaseEnergy(granted)
+    -- Game.ReinitPlayerFromBlackboard()
     return granted
 end
 
@@ -61,11 +63,20 @@ function RandomizerPowerup.ChangeSuit()
     end
 end
 
+function RandomizerPowerup.IncreaseEnergy(item_id)
+    if item_id ~= "ITEM_ENERGY_TANKS" and item_id ~= "ITEM_LIFE_SHARDS" then return end
+    if item_id == "ITEM_LIFE_SHARDS" and (Game.GetItemAmount(Game.GetPlayerName(), item_id) % 4) ~= 0 then return end
+    Game.LogWarn(0, "Increasing player energy.")
+    local max = Game.GetItemAmount(Game.GetPlayerName(), "ITEM_MAX_LIFE")
+    Game.GetPlayer().INVENTORY:SetItemAmount("ITEM_MAX_LIFE", max+100, true)
+    Game.GetPlayer().INVENTORY:SetItemAmount("ITEM_CURRENT_LIFE", max+100, true)
+end
+
 -- Main PBs (always) + PB expansions (if required mains are disabled)
 RandomizerPowerBomb = {}
 setmetatable(RandomizerPowerBomb, {__index = RandomizerPowerup})
 function RandomizerPowerBomb.OnPickedUp(actor, progression)
-    progression = progression or {item_id = "ITEM_WEAPON_POWER_BOMB_MAX"}
+    progression = {{item_id = "ITEM_WEAPON_POWER_BOMB_MAX", quantity = 0}}
     local granted = RandomizerPowerup.OnPickedUp(actor, progression)
     if granted == "ITEM_WEAPON_POWER_BOMB_MAX" then
         Game.GetPlayer().INVENTORY:SetItemAmount("ITEM_WEAPON_POWER_BOMB", 1, true)
