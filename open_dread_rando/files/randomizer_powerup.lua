43e3900a88927dbba26c1dedde0d99920180c656
duncathan <dunc@duncathan.com>
2022-02-07 20:40:40 -0600

    properly fix certain pickups hurting you on collect

--- open_dread_rando/files/randomizer_powerup.lua
+++ open_dread_rando/files/randomizer_powerup.lua
@@ -6,6 +6,26 @@ end
 
 RandomizerPowerup.Self = nil
 
+function RandomizerPowerup.SetItemAmount(item_id, quantity)
+    if type(quantity) == "string" then
+        quantity = RandomizerPowerup.GetItemAmount(quantity)
+    end
+    Game.SetItemAmount(Game.GetPlayerName(), item_id, quantity)
+end
+function RandomizerPowerup.GetItemAmount(item_id)
+    return Game.GetItemAmount(Game.GetPlayerName(), item_id)
+end
+function RandomizerPowerup.IncreaseItemAmount(item_id, quantity, capacity)
+    local target = RandomizerPowerup.GetItemAmount(item_id) + quantity
+    if capacity ~= nil then
+        if type(capacity) == "string" then
+            capacity = RandomizerPowerup.GetItemAmount(capacity)
+        end
+        target = math.min(target, capacity)
+    end
+    RandomizerPowerup.SetItemAmount(item_id, target)
+end
+
 function RandomizerPowerup.OnPickedUp(actor, progression)
     RandomizerPowerup.Self = actor
     local name = "Boss"
@@ -49,10 +69,10 @@ function RandomizerPowerup.ProgressivePickup(actor, progression)
     Game.LogWarn(0, data)
 
     for _, resource in ipairs(progression) do
-        local current = Game.GetItemAmount(Game.GetPlayerName(), resource.item_id)
+        local current = RandomizerPowerup.GetItemAmount(resource.item_id)
         if loop or current < resource.quantity then
             Game.LogWarn(0, "Granting " .. resource.quantity .. " " .. resource.item_id)
-            Scenario.SetItemAmount(resource.item_id, current + resource.quantity)
+            RandomizerPowerup.IncreaseItemAmount(resource.item_id, resource.quantity)
             return resource
         end
     end
@@ -68,7 +88,7 @@ function RandomizerPowerup.ChangeSuit()
     local model_updater = Game.GetPlayer().MODELUPDATER
     for _, suit in ipairs(suits) do
         if suit.model == model_updater.sModelAlias then break end
-        if Game.GetItemAmount(Game.GetPlayerName(), suit.item) > 0 then
+        if RandomizerPowerup.GetItemAmount(suit.item) > 0 then
             Game.LogWarn(0, "Updating suit to " .. suit.model)
             model_updater.sModelAlias = suit.model
             model_updater:ForceUpdate()
@@ -81,11 +101,10 @@ function RandomizerPowerup.IncreaseEnergy(resource)
     if resource == nil then return end
     local item_id = resource.item_id
     if item_id ~= "ITEM_ENERGY_TANKS" and item_id ~= "ITEM_LIFE_SHARDS" then return end
-    if item_id == "ITEM_LIFE_SHARDS" and (Game.GetItemAmount(Game.GetPlayerName(), item_id) % 4) ~= 0 then return end
+    if item_id == "ITEM_LIFE_SHARDS" and (RandomizerPowerup.GetItemAmount(item_id) % 4) ~= 0 then return end
     Game.LogWarn(0, "Increasing player energy.")
-    local max = Game.GetItemAmount(Game.GetPlayerName(), "ITEM_MAX_LIFE")
-    Scenario.SetItemAmount("ITEM_MAX_LIFE", max+100)
-    Scenario.SetItemAmount("ITEM_CURRENT_LIFE", max+100)
+    RandomizerPowerup.IncreaseItemAmount("ITEM_MAX_LIFE", 100)
+    RandomizerPowerup.SetItemAmount("ITEM_CURRENT_LIFE", "ITEM_MAX_LIFE")
 end
 
 function RandomizerPowerup.IncreaseAmmo(resource)
@@ -100,9 +119,7 @@ function RandomizerPowerup.IncreaseAmmo(resource)
 
     if current_id == nil then return end
 
-    local current = Game.GetItemAmount(Game.GetPlayerName(), current_id)
-    local max = Game.GetItemAmount(Game.GetPlayerName(), resource.item_id)
-    Scenario.SetItemAmount(current_id, math.min(max, current + resource.quantity))
+    RandomizerPowerup.IncreaseItemAmount(current_id, resource.quantity, resource.item_id)
 end
 
 -- Main PBs (always) + PB expansions (if required mains are disabled)
@@ -112,14 +129,21 @@ function RandomizerPowerBomb.OnPickedUp(actor, progression)
     progression = {{item_id = "ITEM_WEAPON_POWER_BOMB_MAX", quantity = 0}}
     local granted = RandomizerPowerup.OnPickedUp(actor, progression)
     if granted ~= nil and granted.item_id == "ITEM_WEAPON_POWER_BOMB_MAX" then
-        Scenario.SetItemAmount("ITEM_WEAPON_POWER_BOMB", 1)
+        RandomizerPowerup.SetItemAmount("ITEM_WEAPON_POWER_BOMB", 1)
     end
 end
 
 RandomizerPhantomCloak = {}
 setmetatable(RandomizerPhantomCloak, {__index = RandomizerPowerup})
 function RandomizerPhantomCloak.OnPickedUp(actor, progression)
+    local has_cloak_already = RandomizerPowerup.GetItemAmount("ITEM_OPTIC_CAMOUFLAGE") > 0
     RandomizerPowerup.OnPickedUp(actor, progression)
+    if not has_cloak_already then
+        Game.AddSF(0.101, RandomizerPhantomCloak.Deactivate, "")
+    end
+end
+
+function RandomizerPhantomCloak.Deactivate()
     -- prevent the pickup from trying to kill you
-    Scenario.SetItemAmount("ITEM_CURRENT_SPECIAL_ENERGY", 1000)
+    Game.GetPlayer().SPECIALENERGY:Fill()
 end
