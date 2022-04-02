a92d6a7d49bcbcf52a26f542682d21cb233df943
Arcanox <arcanox@arcanox.me>
2022-04-02 17:50:13 -0500

    Use a new prop in the shared blackboard to track whether or not the rando game has been initialized instead of re-using the Artaria "intro applied" prop in the per-scenario blackboard

--- open_dread_rando/files/custom_scenario.lua
+++ open_dread_rando/files/custom_scenario.lua
@@ -43,11 +43,12 @@ function Scenario.EmmyAbilityObtained_ShowMessage(message, callback, finalcallba
     Game.AddSF(0.5, Game.PlayCurrentEnvironmentMusic, "")
 end
 
+Scenario.sRandoGameInitializedPropID = Blackboard.RegisterLUAProp("RANDO_GAME_INITIALIZED", "bool")
 local init_scenario = Scenario.InitScenario
 function Scenario.InitScenario(arg1, arg2, arg3, arg4)
     init_scenario(arg1, arg2, arg3, arg4)
-    if not Scenario.ReadFromBlackboard(Scenario.LUAPropIDs.CAVES_GAME_INTRO, false) then
-        Scenario.WriteToBlackboard(Scenario.LUAPropIDs.CAVES_GAME_INTRO, "b", true)
+    if not Scenario.ReadFromSharedBlackboard(Scenario.sRandoGameInitializedPropID, false) then
+        Scenario.WriteToSharedBlackboard(Scenario.sRandoGameInitializedPropID, "b", true)
         Game.AddSF(0.9, Init.SaveGameAtStartingLocation, "")
         Game.AddSF(0.8, Scenario.ShowText, "")
     end
