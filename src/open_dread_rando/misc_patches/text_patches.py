7687afadd5a14370d2a355b9d9d2fb3d998ac850
Mayberry <mayberryzoom@gmail.com>
2024-05-08 12:29:47 -0400

    Add support for pages in hints

--- src/open_dread_rando/misc_patches/text_patches.py
+++ src/open_dread_rando/misc_patches/text_patches.py
@@ -59,12 +59,16 @@ def patch_hints(editor: PatcherEditor, hints: list[dict]):
 
         # update hint text
         hint_id = f"DIAG_ADAM_{hint['hint_id']}"
-        string_key = f"{hint_id}_PAGE_1"
+
+        pages = {
+            f"{hint_id}_PAGE_{i + 1}": hint
+            for i, hint in enumerate(hint["text"])
+        }
 
         usable.tCaptionList = {
-            hint_id: [string_key]
+            hint_id: list(pages) or [f"{hint_id}_PAGE_1"]
         }
-        patch_text(editor, string_key, hint["text"])
+        apply_text_patches(editor, pages)
 
 
 _PROJECT_MEMBERS = {
