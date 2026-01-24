9966ceed7fb2fca36536e437cb389d52a21c327d
Arcanox <arcanox@arcanox.me>
2026-01-24 04:21:14 -0600

    Replace Lua-constructed rando GUI elements with a custom BMSCP composition that is loaded and appended to the HUD

--- src/open_dread_rando/files/lua_libraries/disconnect_gui.lua
+++ src/open_dread_rando/files/lua_libraries/disconnect_gui.lua
@@ -1,48 +1,18 @@
 DisconnectGui = DisconnectGui or {
-    ui = nil,
     container = nil,
 }
 
-function DisconnectGui.Init()
-    if DisconnectGui.ui then
-        DisconnectGui.ui:Destroy()
-        DisconnectGui.ui = nil
-        DisconnectGui.container = nil
-    end
-
-    local hud = GUI.GetDisplayObject("IngameMenuRoot.iconshudcomposition")
-    local ui = GUILib("DisconnectUI", hud)
-    local container = ui:AddContainer("Content", {
-        X = 0.025,
-        Y = 0.25,
-        SizeX = 0.2,
-        SizeY = 0.04,
-        Visible = false,
-    })
-
-    container:AddSprite("DisconnetIcon", "HUD_TILESET/DISCONNECT", {
-        X = 0.000,
-        Y = 0.000,
-        SizeX = 0.035,
-        SizeY = 0.045,
-        ColorR = 0.9, ColorG = 0.1, ColorB = 0.1,
-        BlinkColorR = "0.9", BlinkColorG = "0.1", BlinkColorB = "0.1", BlinkAlpha = "0.00000", Blink = "1.00000",
-        Enabled = true,
-    })
-
-    container:AddLabel("DisconnectText", "Disconnected!", {
-        X = "0.042",
-        Y = "0.0",
-        SizeX = "0.3",
-        SizeY = "0.04",
-        Font = "digital_small",
-        Autosize = false,
-    })
-
-    DisconnectGui.ui = ui
-    DisconnectGui.container = container
+function DisconnectGui.Init(panel)
+    DisconnectGui.container = panel
+    GUI.SetProperties(panel:FindChild("DisconnectIcon"), {
+        BlinkColorR = "0.9",
+        BlinkColorG = "0.1",
+        BlinkColorB = "0.1",
+        BlinkAlpha = "0.0",
+        Blink = "1.0",
+     })
 end
 
 function DisconnectGui.Update(visible)
-    DisconnectGui.container:SetProperties({Visible = visible})
+    GUI.SetProperties(DisconnectGui.container, { Visible = visible })
 end
--- src/open_dread_rando/files/lua_libraries/room_names.lua
+++ src/open_dread_rando/files/lua_libraries/room_names.lua
@@ -1,8 +1,8 @@
 Game.DoFile("system/scripts/cc_to_room_name.lc")
 
 RoomNameGui = RoomNameGui or {
-    ui = nil,
     container = nil,
+    label = nil,
     cameraDict = RANDO_CC_DICTIONARY,
     fadeTime = Init.bRoomIdFadeTime,
     fadeOutSFID = nil,
@@ -26,48 +26,10 @@ function RoomNameGui.GetRoomName(camera)
     return rando_name
 end
 
-function RoomNameGui.Init()
-    if RoomNameGui.ui then
-        RoomNameGui.ui:Destroy()
-        RoomNameGui.ui = nil
-        RoomNameGui.container = nil
-    end
-
-    local hud = GUI.GetDisplayObject("IngameMenuRoot.iconshudcomposition")
-    local ui = GUILib("RoomName", hud)
-    local container = ui:AddContainer("Content", {
-        X = 0.025,
-        Y = 0.925,
-        SizeX = 0.2,
-        SizeY = 0.04,
-    })
-
-    container:AddSprite("Background", "HUD_TILESET/BACKGROUND", {
-        SizeX = 0.3,
-        SizeY = 0.06,
-    })
-    container:AddSprite("Frame_top", "HUD_TILESET/FRAME_TOP", {
-        X = -0.0005,
-        Y = -0.0005,
-        SizeX = 0.05,
-        SizeY = 0.015,
-        ColorR = 0.8773584961891174,
-        ColorG = 0.8773584961891174,
-        ColorB = 0.8773584961891174,
-    })
-
-    local label = container:AddLabel("RoomNameGui_Text", "Room: Unknown", {
-        X = "0.0075",
-        Y = "0.01",
-        SizeX = "0.3",
-        SizeY = "0.04",
-        Font = "digital_small",
-        Autosize = false,
-    })
-
-    RoomNameGui.ui = ui
-    RoomNameGui.container = container
-    RoomNameGui.label = label
+function RoomNameGui.Init(panel)
+	GUI.SetProperties(panel, { Visible = true })
+    RoomNameGui.container = panel
+    RoomNameGui.label = panel:FindChild("RoomNameGui_Text")
 
     local current_cc = Game.GetCurrentSubAreaId()
 
@@ -96,7 +58,8 @@ function RoomNameGui.Update(new_cc)
         return
     else
         RoomNameGui.Fade("1.0")
-        label:SetText(room_name)
+        GUI.SetLabelText(label, room_name)
+        label:ForceRedraw()
     end
 
     if RoomNameGui.fadeTime ~= nil and RoomNameGui.fadeTime ~= -1 then
@@ -119,5 +82,5 @@ function RoomNameGui.Fade(fade_val, fade_time)
         return
     end
 
-    container:SetProperties({FadeColorR = "-1.0", FadeColorG = "-1.0", FadeColorB = "-1.0", FadeColorA = fade_val, FadeTime = fade_time})
+    GUI.SetProperties(container, { FadeColorR = "-1.0", FadeColorG = "-1.0", FadeColorB = "-1.0", FadeColorA = fade_val, FadeTime = fade_time })
 end
